a0053a6a290fc38c0acf8c59da6de59db096bc14
Arthur Rio <arthur@punchtab.com>
2013-08-21 10:49:27 -0700

    add the suds methods

--- suds_marketo/__init__.py
+++ suds_marketo/__init__.py
@@ -22,15 +22,20 @@ class Client(object):
 
         self.suds_client = SudsClient('http://app.marketo.com/soap/mktows/2_0?WSDL',
                 location=soap_endpoint)
-        # Make easy the access to the types
-        self.valid_types = []
-        for valid_type in self.suds_client.sd[0].types:
-            self.valid_types.append(valid_type[0].name)
+        # Make easy the access to the types and methods
+        self.suds_types = []
+        self.suds_methods = []
+        for suds_type in self.suds_client.sd[0].types:
+            self.suds_types.append(suds_type[0].name)
+        for suds_method in self.suds_client.sd[0].service.ports[0].binding.operations:
+            self.suds_methods.append(suds_method)
 
     def __getattribute__(self, name):
-        if name != 'valid_types' and name in self.valid_types:
+        if name not in ('suds_types', 'suds_methods') and name in self.suds_types:
             # if the attribute is one of the Types
             return self.suds_client.factory.create(name)
+        elif name not in ('suds_types', 'suds_methods') and name in self.suds_methods:
+            return self.suds_client.service.__getattr__(name)
         else:
             return super(Client, self).__getattribute__(name)
 
@@ -38,8 +43,15 @@ class Client(object):
         soapheaders = auth.header(self.user_id, self.encryption_key)
         self.suds_client.set_options(soapheaders=soapheaders)
 
+
+    def call_service(self, name, *args, **kwargs):
+        self.set_header()
+        self.__getattribute__(name)(*args, **kwargs)
+
     def get_lead(self, email):
         lead_key = self.LeadKey
         lead_key.keyType = self.LeadKeyRef.EMAIL
         lead_key.keyValue = email
-        self.suds_client.service.getLead(lead_key)
+        self.call_service('getLead', lead_key)
+
+
