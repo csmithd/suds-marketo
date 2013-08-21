765d2617ebfbfc84aee26c428d616025cf4bda33
Arthur Rio <arthur@punchtab.com>
2013-08-20 17:24:26 -0700

    change the way we add the prefix

--- suds_marketo/auth.py
+++ suds_marketo/auth.py
@@ -9,7 +9,7 @@ def sign(message, encryption_key):
     return digest.hexdigest().lower()
 
 def header(user_id, encryption_key):
-    authentication_header = Element("AuthenticationHeader").addPrefix(p='ns1')
+    authentication_header = Element("ns1:AuthenticationHeader")
     timestamp = datetime.utcnow().isoformat()
     signature = sign(timestamp + user_id, encryption_key)
 
